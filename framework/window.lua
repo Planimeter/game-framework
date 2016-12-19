22817f5c6dbc7efc1e8399dc7a0424b941caf9e1
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2016-12-19 11:17:09 -0700

    Update framework.window.createWindow()

--- framework/window.lua
+++ framework/window.lua
@@ -8,6 +8,6 @@ local SDL = require( "lib.sdl" )
 
 module( "framework.window" )
 
-function createWindow()
-	window = SDL.SDL_CreateWindow()
+function createWindow( title, x, y, width, height )
+	window = SDL.SDL_CreateWindow( title, x, y width, height, flags )
 end
