c7ad8ad804a7d5e5e95d7f695d0d857c3954bf6c
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2016-12-19 15:19:52 -0700

    Update lsdl

--- lib/sdl.lua
+++ lib/sdl.lua
@@ -7,13 +7,24 @@
 local ffi = require( "ffi" )
 local bit = require( "bit" )
 
-local bor = bit.bor
+local bor  = bit.bor
+local band = bit.band
 
 io.input( "lib/SDL.h" )
 ffi.cdef( io.read( "*all" ) )
 
 local SDL = ffi.load( "SDL2" )
-local _M  = {
+
+local function SDL_WINDOWPOS_UNDEFINED_DISPLAY(X)
+	return (bor(SDL_WINDOWPOS_UNDEFINED_MASK,(X)))
+end
+
+local function SDL_WINDOWPOS_ISUNDEFINED(X)
+	return (band((X),0xFFFF0000) == SDL_WINDOWPOS_UNDEFINED_MASK)
+end
+
+
+local _M = {
 	--[[
 	 *  \name SDL_INIT_*
 	 *
@@ -28,6 +39,14 @@ local _M  = {
 	SDL_INIT_GAMECONTROLLER = 0x00002000,  --[[< SDL_INIT_GAMECONTROLLER implies SDL_INIT_JOYSTICK ]]
 	SDL_INIT_EVENTS         = 0x00004000,
 	SDL_INIT_NOPARACHUTE    = 0x00100000,  --[[< compatibility; this flag is ignored. ]]
+
+	--[[
+	 *  \brief Used to indicate that you don't care what the window position is.
+	 ]]
+	SDL_WINDOWPOS_UNDEFINED_MASK    = 0x1FFF0000,
+	SDL_WINDOWPOS_UNDEFINED_DISPLAY = SDL_WINDOWPOS_UNDEFINED_DISPLAY,
+	SDL_WINDOWPOS_UNDEFINED         = SDL_WINDOWPOS_UNDEFINED_DISPLAY(0),
+	SDL_WINDOWPOS_ISUNDEFINED       = SDL_WINDOWPOS_ISUNDEFINED
 }
 _M.SDL_INIT_EVERYTHING = bor(
                 _M.SDL_INIT_TIMER, _M.SDL_INIT_AUDIO, _M.SDL_INIT_VIDEO, _M.SDL_INIT_EVENTS,
