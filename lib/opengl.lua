3440c63f5ffb7e9ee7d603bc4d5e924ff126b476
Andrew McWatters <andrewmcwatters@Andrews-iMac.local>
2016-12-21 21:06:04 -0700

    Update lopengl

--- lib/opengl.lua
+++ lib/opengl.lua
@@ -7,4 +7,14 @@
 local ffi = require( "ffi" )
 io.input( "lib/glcorearb.h" )
 ffi.cdef( io.read( "*all" ) )
-return ffi.load( "OpenGL.framework/OpenGL" )
+
+local _M = {}
+return setmetatable( _M, {
+	__index = function( table, key )
+		local proc = "gl" .. key
+		local ct   = "PFNGL" .. string.upper( key ) .. "PROC"
+		local cdata = ffi.cast( ct, SDL.SDL_GL_GetProcAddress( proc ) )
+		_M[ key ] = cdata
+		return cdata
+	end
+} )
