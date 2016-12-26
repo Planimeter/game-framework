5d8a178512a4585ea1f6e4f3670a8e4332c51e06
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2016-12-26 12:11:59 -0700

    Update main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+--=========== Copyright © 2016, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+function framework.load()
+end
+
+function framework.update( dt )
+end
+
+function framework.draw()
+end
