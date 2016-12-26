bee5a5ac840451fca560497b077a05397242b6cd
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2016-12-26 15:52:01 -0700

    Add graphics transformation functions

--- framework/graphics/transformation.lua
+++ framework/graphics/transformation.lua
@@ -4,13 +4,10 @@
 --
 --============================================================================--
 
-require( "framework.graphics.shader" )
-require( "framework.graphics.transformation" )
-
-local GL = require( "lib.opengl" )
-
 module( "framework.graphics" )
 
-function clear()
-	GL.glClear( 0x00004000 )
+function push()
+end
+
+function pop()
 end
