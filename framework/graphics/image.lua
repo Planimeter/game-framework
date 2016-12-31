0afb9c31f7de6bec6b662c3ca63835e76995cbaa
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2016-12-31 12:13:28 -0700

    Added DevIL SWIG Lua bindings

--- framework/graphics/image.lua
+++ framework/graphics/image.lua
@@ -7,6 +7,17 @@
 require( "class" )
 local ffi   = require( "ffi" )
 local GL    = require( "lib.opengl" )
+local DevIL = require( "DevIL" )
+
+-- Needed to initialize DevIL.
+DevIL.ilInit ()
+DevIL.iluInit()
+
+-- GL cannot use palettes anyway, so convert early.
+DevIL.ilEnable (DevIL.IL_CONV_PAL)
+
+-- Gets rid of dithering on some nVidia-based cards.
+DevIL.ilutEnable (DevIL.ILUT_OPENGL_CONV)
 
 class( "framework.graphics.image" )
 
