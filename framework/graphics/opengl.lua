07b2cc567b6cd6a58c014af358ba15bfe046a08c
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2017-01-02 11:38:31 -0700

    Update opengl.lua

--- framework/graphics/opengl.lua
+++ framework/graphics/opengl.lua
@@ -9,14 +9,6 @@ local GL  = require( "lib.opengl" )
 
 module( "framework.graphics" )
 
-function getOpenGLVersion()
-	local majorVersion = ffi.new( "GLint[1]" )
-	local minorVersion = ffi.new( "GLint[1]" )
-	GL.glGetIntegerv( 0x821B, majorVersion )
-	GL.glGetIntegerv( 0x821C, minorVersion )
-	return majorVersion[0] .. "." .. minorVersion[0]
-end
-
 function newVertexArray()
 	local vao = ffi.new( "GLuint[1]" )
 	GL.glGenVertexArrays( 1, vao )
