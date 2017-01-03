23cfffdef0930999041d50d9d1be8b290789fa63
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2017-01-02 23:09:27 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,17 +12,9 @@ end
 
 function framework.draw()
 	local width, height = framework.graphics.getSize()
-	framework.graphics.polygon( "line", {
+	framework.graphics.polygon( "fill", {
 	    width / 2,         height / 4,          -- Vertex 1 (X, Y)
 	    width - width / 4, height - height / 4, -- Vertex 2 (X, Y)
 	    width / 4,         height - height / 4  -- Vertex 3 (X, Y)
 	} )
-end
-
-function framework.keypressed( key, scancode, isrepeat )
-	print( key, scancode, isrepeat )
-end
-
-function framework.keyreleased( key, scancode )
-	print( key, scancode )
 end
