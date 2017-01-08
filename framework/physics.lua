387daa5e00654b3e144d571bcc38e2a4352b7ed7
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2017-01-08 11:52:54 -0700

    Add lchipmunk

---
+++ framework/physics.lua
@@ -0,0 +1,9 @@
+--=========== Copyright © 2016, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+local chipmunk = require( "lib.chipmunk" )
+
+module( "framework.physics" )
