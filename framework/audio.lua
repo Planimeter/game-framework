f4a0f4981480348f6e33453cb4938be82365cb46
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2017-01-10 13:28:13 -0700

    Update audio.lua

--- framework/audio.lua
+++ framework/audio.lua
@@ -4,6 +4,8 @@
 --
 --============================================================================--
 
+local AL = require( "lib.openal" )
+
 module( "framework.audio" )
 
 function newSource( filename )
