10a8b2b3377bb4fe4a1289c9724fccac345fbb20
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Air.local>
2017-01-13 08:58:30 -0700

    Add framework.mouse.getPosition()

--- framework/mouse.lua
+++ framework/mouse.lua
@@ -4,9 +4,17 @@
 --
 --============================================================================--
 
+local ffi = require( "ffi" )
 local SDL = require( "sdl" )
 
 module( "framework.mouse" )
+
+function getPosition()
+	local x = ffi.new( "int[1]" )
+	local y = ffi.new( "int[1]" )
+	SDL.SDL_GetMouseState( x, y )
+	return x[0], y[0]
+end
 
 function setVisible( visible )
 	SDL.SDL_ShowCursor( visible and SDL.SDL_ENABLE or SDL.SDL_DISABLE )
