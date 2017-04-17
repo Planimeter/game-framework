a5dca76242a5f4ec996e565ee2e3755a0224d33a
Andrew McWatters <mcwattersandrew@gmail.com>
2017-04-16 17:49:55 -0700

    Update loadlib.lua

--- lua/framework/loadlib.lua
+++ lua/framework/loadlib.lua
@@ -8,16 +8,16 @@
 package.path = string.gsub( package.path, "^./?.lua;", "./?.lua;./?/init.lua;" )
 
 if ( jit.os == "Windows" ) then
-	-- Add framework.path and framework.cpath
+	-- Add `framework.path' and `framework.cpath'
 	framework.path  = string.gsub( arg[ 0 ], "\\framework\\init.lua$", "\\" )
 	framework.cpath = string.gsub( framework.path, "\\lua$", "\\" )
 
-	-- Add lib
+	-- Add `lib'
 	package.path    = package.path  .. framework.path .. "lib\\?.lua;"
 	package.cpath   = package.cpath .. ";".. framework.path .. "lib\\?.dll;"
 	package.cpath   = package.cpath .. framework.path .. "loadall.dll"
 else
-	-- Add framework.path and framework.cpath
+	-- Add `framework.path' and `framework.cpath'
 	framework.path  = string.gsub( arg[ 0 ], "/framework/init.lua$", "/" )
 	framework.cpath = string.gsub( framework.path, "/lua$", "/" )
 
@@ -25,7 +25,7 @@ else
 	package.path    = package.path  .. ";" .. framework.path .. "?.lua;"
 	package.path    = package.path  .. framework.path .. "?/init.lua;"
 
-	-- Add lib
+	-- Add `lib'
 	package.path    = package.path  .. framework.path .. "lib/?.lua"
 	package.cpath   = package.cpath .. ";" .. framework.path .. "lib/?.so;"
 	package.cpath   = package.cpath .. framework.path .. "loadall.so"
