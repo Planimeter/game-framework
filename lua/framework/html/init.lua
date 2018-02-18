bef7f6aa86a5b05b74cd4cd55178e7dde054256f
Andrew McWatters <mcwattersandrew@gmail.com>
2018-02-18 00:45:39 -0700

    Add framework.html

---
+++ lua/framework/html/init.lua
@@ -0,0 +1,25 @@
+--=========== Copyright © 2017, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+require( "framework.html.browser" )
+local cef = require( "cef" )
+
+local framework = framework
+
+module( "framework.html" )
+
+function newBrowser( url )
+	local browser = framework.html.browser
+	return browser( url )
+end
+
+function update( dt )
+	cef.cef_do_message_loop_work()
+end
+
+function quit()
+	cef.cef_shutdown()
+end
