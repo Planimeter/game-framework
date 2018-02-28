4f843094d8996e57aefaf18587ad1e6d285d4d6e
Andrew McWatters <mcwattersandrew@gmail.com>
2018-02-28 08:39:33 -0700

    Add `kazmath.kmMat4Print`

--- lua/lib/kazmath.lua
+++ lua/lib/kazmath.lua
@@ -7,4 +7,24 @@
 local ffi = require( "ffi" )
 io.input( framework.path .. "include/kazmath.h" )
 ffi.cdef( io.read( "*all" ) )
-return ffi.load( ffi.os == "Windows" and "kazmath" or framework.cpath .. "libkazmath.dylib" )
+
+local kazmath = ffi.load(
+	ffi.os == "Windows" and "kazmath" or framework.cpath .. "libkazmath.dylib"
+)
+local _M = {}
+
+function _M.kmMat4Print( pOut )
+	local mat = pOut.mat
+	print(
+		mat[0],  mat[1],  mat[2],  mat[3]          .. "\n",
+		"\r" .. mat[4],  mat[5],  mat[6],  mat[7]  .. "\n",
+		"\r" .. mat[8],  mat[9],  mat[10], mat[11] .. "\n",
+		"\r" .. mat[12], mat[13], mat[14], mat[15]
+	)
+end
+
+return setmetatable( _M, {
+	__index = function( table, key )
+		return kazmath[ key ]
+	end
+} )
