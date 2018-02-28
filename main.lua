77b4c66e6607094cbc975b59ba04f686717b5330
Andrew McWatters <mcwattersandrew@gmail.com>
2018-02-28 14:34:20 -0700

    Update default scene

--- main.lua
+++ main.lua
@@ -4,12 +4,6 @@
 --
 --============================================================================--
 
-local assimp = require( "assimp" )
-local ffi    = require( "ffi" )
-local min    = ffi.new( "struct aiVector3D" )
-local max    = ffi.new( "struct aiVector3D" )
-local center = ffi.new( "struct aiVector3D" )
-
 function framework.load( arg )
 	framework.graphics.setGlTFPBRShader()
 
@@ -22,14 +16,11 @@ function framework.load( arg )
 	} )
 
 	local width, height = framework.graphics.getSize()
-	framework.graphics.setPerspectiveProjection( 45, width / height, 0.1, 1000 )
+	framework.graphics.setPerspectiveProjection( 45, width / height, 0.01, 100 )
 
-	teapot = framework.graphics.newModel( "models/teapot.obj" )
-
-	teapot:getBoundingBox( min, max )
-	center.x = ( min.x + max.x ) / 2
-	center.y = ( min.y + max.y ) / 2
-	center.z = ( min.z + max.z ) / 2
+	helmet = framework.graphics.newModel(
+		"models/DamagedHelmet/gltf/DamagedHelmet.gltf"
+	)
 end
 
 function framework.update( dt )
@@ -38,17 +29,11 @@ end
 function framework.draw()
 	framework.graphics.push()
 		framework.graphics.lookAt(
-			0,  0,  3,
-			0,  0, -5,
-			0,  1,  0
+			0, 0, 4,
+			0, 0, 0,
+			0, 1, 0
 		)
 		framework.graphics.rotateY( math.rad( 180 ) )
-		scale = max.x - min.x
-		scale = math.max( max.y - min.y, scale )
-		scale = math.max( max.z - min.z, scale )
-		scale = 1 / scale
-		framework.graphics.scale( scale, scale, scale )
-		framework.graphics.translate( -center.x, -center.y, -center.z )
-		framework.graphics.draw( teapot )
+		framework.graphics.draw( helmet )
 	framework.graphics.pop()
 end
