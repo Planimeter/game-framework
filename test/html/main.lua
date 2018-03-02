b02a37e0c85868d2eaf209ed351512d18ffac24a
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-02 12:17:57 -0700

    Add tests

---
+++ test/html/main.lua
@@ -0,0 +1,23 @@
+--=========== Copyright © 2018, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+require( "framework.html" )
+
+function framework.load( arg )
+	browser = framework.html.newBrowser( "https://www.google.com/ncr" )
+end
+
+function framework.resize( width, height )
+	browser:resize( width, height )
+end
+
+function framework.update( dt )
+	framework.html.update( dt )
+end
+
+function framework.draw()
+	framework.graphics.draw( browser )
+end
