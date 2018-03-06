09677ee34327754321b64905dce9e579dfc6ca6b
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-06 14:22:26 -0700

    Add compat module for LÖVE

---
+++ lua/framework/compat.lua
@@ -0,0 +1,9 @@
+--=========== Copyright © 2018, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+local framework = framework
+
+module( "framework.love" )
