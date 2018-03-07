43fb570f70b56108731a44a613dcc114853d8fa4
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-06 21:43:34 -0700

    Add `love.system.getOS`

--- lua/framework/love.lua
+++ lua/framework/love.lua
@@ -4,7 +4,21 @@
 --
 --============================================================================--
 
-local framework = framework
-love = framework
+local jit       = jit
 
-module( "framework.love" )
+local framework = framework
+_G.love         = framework
+
+local function system()
+	module( "love.system" )
+
+	function getOS()
+		if ( jit.os == "OSX" ) then
+			return "OS X"
+		elseif ( jit.os == "Other" ) then
+			return "Unknown"
+		else
+			return jit.os
+		end
+	end
+end system()
