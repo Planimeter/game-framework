cd65dc2b81bd396d06dbd909b4b1a7e8012ebd6e
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-21 12:15:03 -0700

    Add depth shader

--- lua/framework/graphics/shaders/depth.lua
+++ lua/framework/graphics/shaders/depth.lua
@@ -10,37 +10,30 @@ local kazmath = require( "kazmath" )
 
 return function()
 	-- shader
-	local fragmentSource = framework.filesystem.read( "shaders/skybox.frag" )
-	local vertexSource   = framework.filesystem.read( "shaders/skybox.vert" )
+	local fragmentSource = framework.filesystem.read( "shaders/depth.frag" )
+	local vertexSource   = framework.filesystem.read( "shaders/depth.vert" )
 	local shader         = framework.graphics.newShader( fragmentSource, vertexSource )
-	GL.glBindFragDataLocation( shader, 0, "FragColor" )
+	GL.glBindFragDataLocation( shader, 0, "FragDepth" )
 	framework.graphics.linkShader( shader )
 	framework.graphics.setShader( shader )
 
-	-- skybox.frag
-	-- uniforms
-	-- tex
-	framework.graphics.setDefaultTexture()
-
-	-- skybox.vert
+	-- depth.vert
 	-- attribs
 	-- position
 	local position = GL.glGetAttribLocation( shader, "position" )
-	local stride = ( 3 + 3 ) * ffi.sizeof( "GLfloat" )
+	local stride = 3 * ffi.sizeof( "GLfloat" )
 	GL.glEnableVertexAttribArray( position )
 	GL.glVertexAttribPointer( position, 3, GL.GL_FLOAT, 0, stride, nil )
 
-	-- texcoord
-	local texcoord = GL.glGetAttribLocation( shader, "texcoord" )
-	GL.glEnableVertexAttribArray( texcoord )
-	local pointer = ffi.cast( "GLvoid *", ( 3 + 3 ) * ffi.sizeof( "GLfloat" ) )
-	GL.glVertexAttribPointer( texcoord, 3, GL.GL_FLOAT, 0, stride, pointer )
-
 	-- uniforms
-	-- view
-	local view = GL.glGetUniformLocation( shader, "view" )
+	-- model
+	local model = GL.glGetUniformLocation( shader, "model" )
 	local mat4 = ffi.new( "kmMat4" )
 	kazmath.kmMat4Identity( mat4 )
+	GL.glUniformMatrix4fv( model, 1, GL.GL_FALSE, mat4.mat )
+
+	-- view
+	local view = GL.glGetUniformLocation( shader, "view" )
 	GL.glUniformMatrix4fv( view, 1, GL.GL_FALSE, mat4.mat )
 
 	-- projection
