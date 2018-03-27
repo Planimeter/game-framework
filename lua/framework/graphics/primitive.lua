580aa7fb26143ab563e2d5a6a7bc8cf1afcb336d
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-27 07:43:57 -0700

    Promote `lines` from LÖVE compatibility

--- lua/framework/graphics/primitive.lua
+++ lua/framework/graphics/primitive.lua
@@ -12,6 +12,24 @@ local framework = framework
 
 module( "framework.graphics" )
 
+function line( ... )
+	local vertices       = { ... }
+	local defaultVBO     = framework.graphics._defaultVBO
+	local pVertices      = ffi.new( "GLfloat[?]", #vertices, vertices )
+	local size           = ffi.sizeof( pVertices )
+	local shader         = framework.graphics.getShader()
+	local position       = GL.glGetAttribLocation( shader, "position" )
+	local texcoord       = GL.glGetAttribLocation( shader, "texcoord" )
+	local defaultTexture = framework.graphics.getDefaultTexture()
+	GL.glBindBuffer( GL.GL_ARRAY_BUFFER, defaultVBO[0] )
+	GL.glBufferData( GL.GL_ARRAY_BUFFER, size, pVertices, GL.GL_STREAM_DRAW )
+	GL.glVertexAttribPointer( position, 2, GL.GL_FLOAT, 0, 0, nil )
+	GL.glDisableVertexAttribArray( texcoord )
+	framework.graphics.updateTransformations()
+	GL.glBindTexture( GL.GL_TEXTURE_2D, defaultTexture[0] )
+	framework.graphics.drawArrays( GL.GL_LINES, 0, #vertices / 2 )
+end
+
 function polygon( mode, vertices )
 	if ( mode == "line" ) then
 		mode = GL.GL_LINE_LOOP
