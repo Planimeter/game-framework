16144c2d35ca6cc4f0a323482da1e107ccae2fac
Andrew McWatters <mcwattersandrew@gmail.com>
2018-04-21 08:21:52 -0700

    Enable highdpi mode by default

---
+++ conf.lua
@@ -0,0 +1,9 @@
+--=========== Copyright © 2018, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+function framework.conf( c )
+	c.window.highdpi = true
+end
