3a70a3d8c7a6cbd1f15a71a9ff9d8fff1978f15a
Andrew McWatters <mcwattersandrew@gmail.com>
2018-04-21 15:23:50 -0700

    Remove conf.lua

--- conf.lua
+++
@@ -1,9 +0,0 @@
---=========== Copyright © 2018, Planimeter, All rights reserved. =============--
---
--- Purpose:
---
---============================================================================--
-
-function framework.conf( c )
-	c.window.highdpi = true
-end
