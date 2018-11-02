a239455bb6bafd115603026fed8ae23525b30565
Andrew McWatters <mcwattersandrew@gmail.com>
2018-11-01 22:18:22 -0700

    Update resource handler

--- lua/framework/html/resource.lua
+++ lua/framework/html/resource.lua
@@ -6,13 +6,19 @@
 
 require( "class" )
 local ffi = require( "ffi" )
+local cef = require( "cef" )
 
 class( "framework.html.browser" )
 
 local browser = framework.html.browser
+local toutf8  = framework.html.toutf8
 
 local function process_request( self, request, callback )
-	-- TODO: Handle request.
+	local tmp = request.get_url( request )
+	local url = toutf8( tmp )
+	url = string.gsub( url, "client://lgf/", "" )
+	ffi.C.printf( "%s\n", url )
+	cef.cef_string_userfree_utf16_free( tmp )
 	return 0
 end
 
