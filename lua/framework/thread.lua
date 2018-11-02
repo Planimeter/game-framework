cb314ad8c0541792b9a296b141c3177c3be04521
Andrew McWatters <mcwattersandrew@gmail.com>
2018-11-02 10:01:55 -0700

    Add thread module

---
+++ lua/framework/thread.lua
@@ -0,0 +1,20 @@
+--=========== Copyright © 2018, Planimeter, All rights reserved. =============--
+--
+-- Purpose:
+--
+--============================================================================--
+
+local ffi = require( "ffi" )
+
+module( "framework.thread" )
+
+CriticalSection = CriticalSection or ffi.new( "CRITICAL_SECTION[1]" )
+ffi.C.InitializeCriticalSection( CriticalSection[0] )
+
+function lock()
+	ffi.C.EnterCriticalSection( CriticalSection[0] )
+end
+
+function unlock()
+	ffi.C.LeaveCriticalSection( CriticalSection[0] )
+end
